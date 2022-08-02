ccdfda5ffe6f0313dc1dcc8ff26b003280d186a4
aspwil <aspwil@users.noreply.github.com>
2022-08-01 19:15:11 -0600

    Initial commit

---
+++ src/main/java/io/github/aspwil/craftability/Craftability.java
@@ -0,0 +1,17 @@
+package io.github.aspwil.craftability;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Craftability extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
